26edd0b62e928dc28ca402fed08dfcfffd3cd115
cormacpayne <cormacpayne@ufl.edu>
2014-05-04 18:25:16 -0400

    Update BreadthFirstSearch.java

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-class BreadthFirstSearch{
+class BreadthFirstSearch {
 
 	/* Returns true if the graph is a tree. By definition, a tree is a graph that
 	* doesn't contain a cycle and is minimally connected, that is, if we remove any 
@@ -12,7 +12,7 @@ class BreadthFirstSearch{
 	* nodes in the graph will have been visited, or there are nodes that haven't been
 	* visited yet, proving that the given graph is not a tree.
 	*/
-	public static boolean BFS(ArrayList<ArrayList<Integer>> graph, int vertices){
+	public static boolean BFS(ArrayList<ArrayList<Integer>> graph, int vertices) {
 		// visited keeps track of which nodes we have visited already
 		boolean[] visited = new boolean[vertices+1];
 		// queue will keep track of which nodes we are visiting next
@@ -23,7 +23,7 @@ class BreadthFirstSearch{
 		visited[1] = true;
 
 		// while the queue is not empty, continue to search the tree
-		while(!queue.isEmpty()){
+		while (!queue.isEmpty()) {
 			// current keeps track of the node we are currently on
 			int current = queue.remove();
 			// neighbors gives us a list of the neighbors of the current node
@@ -31,7 +31,7 @@ class BreadthFirstSearch{
 
 			// iterate through each neighbor of the current node and see if we
 			// have visited the neighboring node; if not, add it to the queue.
-			for(int i = 0; i < neighbors.size(); i++){
+			for (int i = 0; i < neighbors.size(); i++) {
 				// neighbor keeps track of which neighbor of the current node
 				// we are currently looking at
 				int neighbor = neighbors.get(i);
@@ -48,7 +48,7 @@ class BreadthFirstSearch{
 
 		// iterate through the nodes and if there is any node we have yet to visit,
 		// then the graph is not a tree and we return false
-		for(int j = 1; j <= vertices; j++){
+		for (int j = 1; j <= vertices; j++) {
 			if(!visited[j]){
 				return false;
 			}
@@ -58,36 +58,36 @@ class BreadthFirstSearch{
 		return true;
 	}
 
-	public static void main(String[] args)throws Exception{
+	public static void main(String[] args) throws Exception {
 		Scanner in = new Scanner(System.in);
 		int vertices = scan(in);
 		int edges = scan(in);
 
-		if(edges + 1 != vertices){
+		if (edges + 1 != vertices) {
 			System.out.print("NO");
-		}else{
+		} else {
 			ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
 
-			for(int i = 0; i <= vertices; i++){
+			for (int i = 0; i <= vertices; i++) {
 				graph.add(new ArrayList<Integer>());
 			}
 
-			for(int j = 0; j < edges; j++){
+			for (int j = 0; j < edges; j++) {
 				int vertex1 = scan(in);
 				int vertex2 = scan(in);
 				graph.get(vertex1).add(vertex2);
 				graph.get(vertex2).add(vertex1);
 			}
 
-			if(BFS(graph, vertices)){
+			if (BFS(graph, vertices)) {
 				System.out.print("YES");
-			}else{
+			} else {
 				System.out.print("NO");
 			}
 		}
 	}
 
-	public static int scan(Scanner in) throws Exception{
+	public static int scan(Scanner in) throws Exception {
 		return Integer.parseInt(in.next());
 	}
 }
